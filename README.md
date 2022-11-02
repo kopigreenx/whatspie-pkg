c4d42490b300d872a9672d3ffd78a7789fddc4af
Mokhamad Rizkianto <mokhamad.rizkianto@gmail.com>
2022-11-02 10:24:17 +0700

    add new fucntion create ticket as agent

--- README.md
+++ README.md
@@ -1,31 +1,74 @@
+
 # Sociomile Digital API Connector
-## 3rd party package for creating Ticket & Check Status Ticket
+
+3rd party package for creating Ticket & Check Status Ticket3rd party package for creating Ticket & Check Status Ticket
+
 
 ## Installation
 
-```sh
+```bash
 composer require kopigreenx/sociomile-digital-pkg
 
 php artisan migrate
-
 ```
 
-## Function
 
-add anywhere you want run 
-```sh
-use Kopigreenx\SociomileDigital\SociomileDigital;
+## Features
+
+- Create Ticket As Agent
+- Create Ticket As Customer
+- Check Status Ticket
+
+
+## Function Reference
+
+#### Create Ticket As Agent
+
+```php
+  SociomileDigital::createTicketAsAgent($internal_id,$phone,$name,$message)
 ```
 
-| function |Required| Description | Status|
-| ------ | ------ | ------ | ------ |
-| SociomileDigital::createTicket | internal_id,user_id,phone,name,message | Creating ticket to sociomile | Active|
-| SociomileDigital::statusTicket | user_id | Check sociomile ticket status | Inactive |
+| Parameter | Type     | Description                |
+| :-------- | :------- | :------------------------- |
+| `$internal_id` | `string` | **Required**. Internal ID |
+| `$phone` | `string` | **Required**. Contact Number  |
+| `$name` | `string` | **Required**. Contact Name  |
+| `$message` | `string` | **Required**. Message Will be sent  |
 
-## IMPORTANT ADD TO Environment File
+#### Create Ticket As Agent
+
+```php
+  SociomileDigital::createTicketAsCustomer($internal_id,$phone,$name,$message)
+```
+
+| Parameter | Type     | Description                |
+| :-------- | :------- | :------------------------- |
+| `$internal_id` | `string` | **Required**. Internal ID |
+| `$phone` | `string` | **Required**. Contact Number  |
+| `$name` | `string` | **Required**. Contact Name  |
+| `$message` | `string` | **Required**. Message Will be sent  |
+
+#### Check Status Ticket
+
+```php
+  SociomileDigital::statusTicket($phone)
+```
+
+| Parameter | Type     | Description                |
+| :-------- | :------- | :------------------------- |
+| `$phone` | `string` | **Required**. Contact Number  |
+
+
+
+
+## Environment Variables
+
 ```sh
 SOCIOMILE_DIGITAL_HOST = <HOST>
 SOCIOMILE_DIGITAL_SECRET = <SECRET_KEY>
 SOCIOMILE_DIGITAL_ID = <SECRET_ID>
+SOCIOMILE_DIGITAL_WABA_ID = <WABA_ID>
+SOCIOMILE_DIGITAL_WABA_TYPE = <TYPE>
+SOCIOMILE_DIGITAL_WABA_SECRET=<WABA_SECRET>
 ```
 
--- src/SociomileDigital.php
+++ src/SociomileDigital.php
@@ -8,7 +8,7 @@ use Kopigreenx\SociomileDigital\Models\SociomileDigitalLog;
 class SociomileDigital
 {
 
-    public static function createTicket(String $internal_id,String $user_id,String $name,String $phone,String $message)
+    public static function createTicketAsCustomer(String $internal_id,String $phone,String $name,String $message)
     {
         $log = new SociomileDigitalLog();
         $client = new Client(['base_uri' => "https://".env("SOCIOMILE_DIGITAL_HOST","XXXXX")."/"]);
@@ -16,7 +16,7 @@ class SociomileDigital
             'Accept'        => 'application/json',
         ];
         $log->internal_id = $internal_id;
-        $log->user_id = $user_id;
+        $log->user_id = $phone;
         $log->name = $name;
         $log->phone = $phone;
         $log->message = $message;
@@ -29,7 +29,7 @@ class SociomileDigital
                         'form_params' => array(
                             'client_secret_key' => env("SOCIOMILE_DIGITAL_SECRET","XXXXX"),
                             'client_secret_id' => env("SOCIOMILE_DIGITAL_ID","XXXXX"),
-                            'user_id' => $user_id,
+                            'user_id' => $phone,
                             'name' => $name,
                             'phone' => $phone,
                             'message' => $message
@@ -73,5 +73,76 @@ class SociomileDigital
         } catch (GuzzleException $e) {
             return response()->json(["status"=>"error","message"=>$e->getMessage()]);
         }
+    }
+
+    public function createTicketAsAgent($internal_id,$phone,$name,$message){
+        $log = new SociomileDigitalLog();
+        $client = new Client(['base_uri' => "https://".env("SOCIOMILE_DIGITAL_HOST","XXXXX")."/"]);
+        $headers = [
+            'Accept'        => 'application/json',
+            'X-API-KEY'        =>  env("SOCIOMILE_DIGITAL_WABA_SECRET","XXXXX"),
+            'X-WABA-ID'        =>  env("SOCIOMILE_DIGITAL_WABA_ID","XXXXX"),
+            'X-NOTIFICATION'        =>  env("SOCIOMILE_DIGITAL_WABA_TYPE","XXXXX"),
+        ];
+        $log->internal_id = $internal_id;
+        $log->user_id = $phone;
+        $log->name = $name;
+        $log->phone = $phone;
+        $log->message = $message;
+
+        $randomId = self::generateRandomString();
+        try {
+            $response = $client->request(
+                'POST',
+                'ivowaba',
+                array(
+                    'headers' => $headers,
+                    'json' => [
+                        "message" => [
+                            "text" => $message
+                        ],
+                        "contact" => [
+                            "id" => "606bc9bab2763d7a3078a3a3",
+                            "full_name" => $name,
+                            "whatsapp" => $phone,
+                            "whatsapp_id" => $phone
+                        ],
+                        "disposition" =>[
+                            "http_code" => 201,
+                            "http_response" => [
+                                "messages" => [
+                                    [
+                                        "id" => $randomId
+                                    ]
+                                ],
+                                "meta" => [
+                                    "api_status" => "stable",
+                                    "version" => "2.33.3"
+                                ]
+                            ],
+                            "message_id" => $randomId
+                        ],
+                    ],
+                )
+            );
+            $body = json_decode($response->getBody());
+            $log->response = json_encode($body);
+            $log->save();
+            return $body;
+        } catch (GuzzleException $e) {
+            $log->response = json_encode(["status"=>"error","message"=>$e->getMessage()]);
+            $log->save();
+            return (object)(["error"=> true,"status"=>"error","message"=>$e->getMessage()]);
+        }
+    }
+
+    protected static function generateRandomString($length = 28) {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
     }
 }
